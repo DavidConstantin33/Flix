68454069c0876c2004965721e6393b95eb35c013
David <142396942+DavidConstantin33@users.noreply.github.com>
2024-03-16 09:28:54 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const acc1 = {
+    username: '33',
+    password: '33',
+}
+
+const acc2 = {
+    username: 'CrissT',
+    password: 'motonel123',
+}
+
+const accs = [ acc1, acc2 ];
+
+const body = document.body;
+const userInput = document.getElementById('username');
+const passInput = document.getElementById('password');
+const loginBtn = document.getElementById('loginBtn');
+const loginWindow = document.querySelector('.login');
+const loginParent = document.querySelector('.loginParent');
+
+
+const login = function () {
+    for (const acc of Object.values(accs)) {
+        if (userInput.value === acc.username && passInput.value === acc.password) {
+            body.classList.add('logged-body');
+            loginWindow.classList.add('hide');
+            loginParent.style.marginTop = '0em';
+        }
+    }
+}
+
+loginBtn.addEventListener('click', login);
